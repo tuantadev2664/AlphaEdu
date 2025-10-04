ffe6520a5de1fe63b0073c3f8bed20e4ecda0131
Trương Anh Tuấn <134388467+tuantadev2664@users.noreply.github.com>
2025-10-05 01:28:13 +0700

    Enhance token validation and authentication logic (#6)
    * Enhance token validation and authentication logic
    
    - Added detailed logging for token validation outcomes in useCurrentUserQuery and useAuth hooks.
    - Updated retry logic to prevent retries on authentication errors and reduced maximum retries.
    - Implemented immediate clearing of invalid tokens and user data in validateToken function, with client-side redirection to sign-in on failure.
    - Introduced a new option to prevent caching on errors in the query configuration.
    
    * Update theme handling in RootLayout component
    
    - Enhanced theme management by adding/removing 'dark' class on the document element based on localStorage settings.
    - Updated meta theme color dynamically for both dark and light themes.
    - Changed default theme in ThemeProvider from 'system' to 'light' for improved user experience.
    
    ---------
    
    Co-authored-by: sawsew467 <thangtvb.dev@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,11 @@ export default async function RootLayout({
             __html: `
               try {
                 if (localStorage.theme === 'dark' || ((!('theme' in localStorage) || localStorage.theme === 'system') && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-                  document.querySelector('meta[name="theme-color"]').setAttribute('content', '${META_THEME_COLORS.dark}')
+                  document.documentElement.classList.add('dark');
+                  document.querySelector('meta[name="theme-color"]').setAttribute('content', '${META_THEME_COLORS.dark}');
+                } else {
+                  document.documentElement.classList.remove('dark');
+                  document.querySelector('meta[name="theme-color"]').setAttribute('content', '${META_THEME_COLORS.light}');
                 }
               } catch (_) {}
             `
@@ -60,7 +64,7 @@ export default async function RootLayout({
         <NuqsAdapter>
           <ThemeProvider
             attribute='class'
-            defaultTheme='system'
+            defaultTheme='light'
             enableSystem
             disableTransitionOnChange
             enableColorScheme
